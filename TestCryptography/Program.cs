8fb3c3876802baa7b6189ccadeb3af7ff83cb508
Dimitrios Zervas <dimitri_zervas@hotmail.com>
2024-03-08 19:35:55 +0200

    Added encrypt/decrypt

--- TestCryptography/Program.cs
+++ TestCryptography/Program.cs
@@ -1,9 +1,43 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Encodings.Web;
 using TestCryptography;
 
 internal class Program
-{ 
+{
+    public static byte[] Decrypt(byte[] encryptedData, byte[] key, byte[] src, byte[] tag)
+    {
+        var ciphertext = encryptedData;// encryptedData[0..^16];
+        //var tag = new byte[16];// encryptedData[^16..];
+        byte[] decrytedBytes = new byte[ciphertext.Length];
+        try
+        {
+            var aes = new AesGcm(key);
+            
+            byte[] iv = new byte[12];
+            Array.Copy(src, iv, 8);
+
+            aes.Decrypt(iv, ciphertext, tag, decrytedBytes);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+        }
+
+        return decrytedBytes;
+    }
+
+    public static void Encrypt(byte[] plainBytes, byte[] key, byte[] src, ref byte[] ciphertext, ref byte[] tag)
+    {
+        var aes = new AesGcm(key);
+
+        byte[] iv = new byte[12];
+        Array.Copy(src, iv, 8);
+        
+        aes.Encrypt(iv, plainBytes, ciphertext, tag);       
+    } 
 
     private static void Main(string[] args)
     {
@@ -29,5 +63,21 @@ internal class Program
             Console.WriteLine($"signs[{i}] Key: {KeyDerivation.ByteArrayToString(signs[i])}");
             Console.WriteLine();
         }
+
+        var data = "Test Ecryption";
+        var dataBytes = Encoding.UTF8.GetBytes(data);
+
+        Console.WriteLine($"Data bytes: {KeyDerivation.ByteArrayToString(dataBytes)}");
+
+        byte[] encryptedShard = new byte[dataBytes.Length];
+        byte[] tag = new byte[16];
+        Encrypt(dataBytes, encrypts[1], src, ref encryptedShard, ref tag);
+
+        Console.WriteLine($"Encrypted bytes: {KeyDerivation.ByteArrayToString(encryptedShard)}");
+        Console.WriteLine($"Calculated Tag: {KeyDerivation.ByteArrayToString(tag)}");
+
+        var decryptedShard = Decrypt(encryptedShard, encrypts[1], src, tag);
+
+        Console.WriteLine($"Encrypted bytes: {KeyDerivation.ByteArrayToString(decryptedShard)}");
     }
 }
